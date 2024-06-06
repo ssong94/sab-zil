be4e0c906c6a05855cc59f7494e9b2f21a73ad66
ssong94 <kyugwang@gmail.com>
2024-06-06 23:48:32 +0900

    first commit

---
+++ src/main/java/com/spring/boot/sabzil/SabZilApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.sabzil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabZilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabZilApplication.class, args);
+	}
+
+}
